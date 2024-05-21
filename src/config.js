bd9aa0fea38eb311de7aab8603a5f8a4f950eef2
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-05-21 21:24:54 +0530

    fix the mongodb connection  error

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const connect = mongoose.connect("mongodb://localhost:27017/Smms");
+const connect = mongoose.connect("mongodb://0.0.0.0/Smms");
 
 connect.then(() => {
     console.log("database connected successfully");
 })
 .catch(() => {
-    console.log("Database cannot connect")
+    console.log("Database cannot connect");
 })
 
 const LoginScema = new mongoose.Schema({
