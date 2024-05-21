9ec8f5edba28783aa0b16cff6c6cb7414f9fef22
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-05-21 22:40:09 +0530

    make some simple backend process to login and signup pages

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const port = 8080
 app.use(express.json())
 app.set('view engine','ejs')
 app.use(express.static("./public"))
+app.use(express.urlencoded({extended: false}))
 
 app.get("/",(req,res) => {
     res.render("home");
@@ -28,6 +29,50 @@ app.get("/admin",(req,res) => {
 
 app.get("/home",(req,res) => {
     res.render("home");
+})
+
+app.post("/login", async (req,res) => {
+   try{
+        const check =await collection.findOne({name: req.body.username});
+        if(!check){
+            res.send("user name cannot find");
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password,check.password);
+        if(isPasswordMatch){
+            res.render("home");
+        }
+        else{
+            res.send("wrong password");
+        }
+   }
+   catch{
+    res.send("wrong detail");
+   }
+
+})
+
+app.post("/signup", async (req,res) => {
+    const data = {
+       name: req.body.username,
+       password: req.body.password
+    }
+    const existinguser = await collection.findOne({name: data.name});
+    if(existinguser){
+        res.send("user alredy exist");
+    }
+    else{
+        //hash password using bcrypt
+        const saltRounds = 10;// no of salt round for bcrypt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+        data.password = hashedPassword;
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+    }
+ 
+    
+
+   
 })
 
 app.listen(port,() =>{
